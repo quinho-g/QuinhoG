3790574bbe781cf4b87aedc9b0232ffe29c4e453
GitQuinho <marcos.paquito2020@gmail.com>
2025-01-31 17:32:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function isInAppBrowser() {return /Instagram|FBAN|FBAV/.test(navigator.userAgent);}if (isInAppBrowser()) {window.location.href = "http://quinho-g.github.io/QuinhoG/";}
+document.addEventListener('touchmove', (buba)=> { const toc = buba.touches[0]
+const pike = toc.pageX;const plke = toc.pageY ; const ins = document.createElement('div') ; ins.classList.add('peef-plef-mnkz')
+ins.style.left = `${pike - 55}px`;ins.style.top = `${plke - 25}px`;document.body.appendChild(ins);ins.addEventListener('animationend', ()=> { ins.remove()}) });
+document.addEventListener('mousemove', (buba)=> { const pike = buba.pageX;const plke = buba.pageY ; const ins = document.createElement('div') ; ins.classList.add('peef-plef-mnkz')
+ins.style.left = `${pike - 55}px`;ins.style.top = `${plke - 25}px`;document.body.appendChild(ins);ins.addEventListener('animationend', ()=> { ins.remove()}) });setInterval (() => { let wuv = document.createElement('div')
+let uo = document.createElement('img');uo.setAttribute('src', 'lob.png');uo.style.width = '80%' ; uo.style.height = '80%' ; uo.style.filter = 'drop-shadow(0 0 0.8vw rgb(174, 0, 255))'
+wuv.classList.add('pfet-pelr-mmkz');wuv.classList.add('pfte-pelr-mhnz');wuv.appendChild(uo) ; wuv.addEventListener('click', () => { wuv.style.backgroundColor = 'white';uo.setAttribute('src', 'lop.png')});document.body.appendChild(wuv)
+wuv.addEventListener('animationend', () => { wuv.remove()})}, 1340) ; setInterval (() => {let ere = document.createElement('div');let opk = document.createElement('img');opk.setAttribute('src', 'lob.png') ; opk.style.width = '80%'
+opk.style.height = '80%' ; opk.style.filter = 'drop-shadow(0 0 0.8vw rgb(174, 0, 255))';ere.classList.add('pfet-pelr-mmkz');ere.classList.add('peff-pelr-mnhz') ; ere.appendChild(opk) ; ere.addEventListener('click', () => { ere.style.backgroundColor = 'white'
+opk.setAttribute('src', 'lop.png')}) ; document.body.appendChild(ere) ; ere.addEventListener('animationend', () => { ere.remove()})}, 1850);setInterval (() => { let ljh = document.createElement('div'); let rmk = document.createElement('img');rmk.setAttribute('src', 'lob.png')
+rmk.style.width = '80%';rmk.style.height = '80%'; rmk.style.filter = 'drop-shadow(0 0 0.8vw rgb(174, 0, 255))';ljh.classList.add('pfet-pelr-mmkz');ljh.classList.add('peft-belr-mhnz');ljh.appendChild(rmk);ljh.addEventListener('click', () => {ljh.style.backgroundColor = 'white'
+rmk.setAttribute('src', 'lop.png')});document.body.appendChild(ljh);ljh.addEventListener('animationend', () => {ljh.remove()})}, 2240)
